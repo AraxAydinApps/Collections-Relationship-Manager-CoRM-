f96394d8bdf8c055199594a5b5a4512c907e7545
AraxAydinApps <araxaydin@gmail.com>
2026-02-05 16:16:09 +0300

    Create corm_config.js

---
+++ corm_config.js
@@ -0,0 +1,8 @@
+// corm_config.js (served from GitHub)
+
+window.CORM_WEBAPP_URL  = "/api/corm"; 
+// اگر بعداً خواستی مستقیم به Apps Script بزنی:
+// window.CORM_WEBAPP_URL = "https://script.google.com/macros/s/XXXXX/exec";
+
+window.CORM_LOGIN_PAGE  = "/index.html";
+window.CORM_LIST_PAGE   = "/dosyalar.html";
